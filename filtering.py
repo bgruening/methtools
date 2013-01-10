1293f99860f27a9af8ee5fd36cc6085bf82278ef
Björn Grüning <bjoern.gruening@gmail.com>
2013-01-10 21:00:09 +0100

    add --quantil option and --min-delta-methylation

--- filtering.py
+++ filtering.py
@@ -5,6 +5,7 @@ import os, sys
 import argparse
 import numpy as np
 from itertools import izip
+from scipy.stats.mstats import mquantiles
 from scipy import stats
 try:
     import fisher as fisher_exact
@@ -16,7 +17,15 @@ except:
     That file needs intersected inputfiles, so that each site is present in both files, affected and control.
 """
 
-def filtering(control_file, affected_file, filtered_control_file, filtered_affected_file, max_pvalue = None, min_cov = None, max_cov = None, min_delta_methylation = None):
+def filtering(control_file, affected_file, filtered_control_file, filtered_affected_file, max_pvalue = None, min_cov = None, max_cov = None, min_delta_methylation = None, filter_quantil = None):
+
+    control_quantil = None
+    affected_quantil = None
+    if filter_quantil:
+        control_quantil = mquantiles( np.loadtxt(control_file, delimiter='\t', usecols=(3,)), prob = [filter_quantil])[0]
+        affected_quantil = mquantiles( np.loadtxt(affected_file, delimiter='\t', usecols=(3,)), prob = [filter_quantil])[0]
+        print control_quantil, affected_quantil
+
     for control_line, affected_line in izip(open(control_file), open(affected_file)):
         c_chrom, c_start, c_end, c_cov, c_meth, c_strand = control_line.strip().split('\t')
         a_chrom, a_start, a_end, a_cov, a_meth, a_strand = affected_line.strip().split('\t')
@@ -34,6 +43,8 @@ def filtering(control_file, affected_file, filtered_control_file, filtered_affec
         if max_cov != None and (a_cov > max_cov or c_cov > max_cov):
             continue
         if min_delta_methylation != None and abs(a_meth - c_meth) < min_delta_methylation:
+            continue
+        if filter_quantil and (c_cov > control_quantil or a_cov > affected_quantil):
             continue
 
         if max_pvalue != None:
@@ -80,13 +91,17 @@ if __name__ == '__main__':
     parser.add_argument("--max-coverage", dest="max_cov", default=None, type=int,
                     help="maximal allowed coverage")
 
-    parser.add_argument("--min-delta-methylation", dest="min_delta_methylation", default=25, type=float,
-                    help="minimal delta between the two mehylation states (default:25)")
+    parser.add_argument("--min-delta-methylation", dest="min_delta_methylation", default=None, type=float,
+                    help="Minimal delta between the two mehylation states")
+
+    parser.add_argument("--quantil", dest="filter_quantil", default=None, type=float,
+                    help="coverage quantil filter, example for the 99.9 quantil: 0.999")
+
 
     options = parser.parse_args()
-    if [options.pvalue, options.min_cov, options.max_cov].count(None) == 3:
-        sys.exit('You need to specify at least one filter parameter: --pvalue, --min-coverage or --max-coverage')
-    filtering(options.control, options.affected, options.ocontrol, options.oaffected, options.pvalue, options.min_cov, options.max_cov, options.min_delta_methylation)
+    if [options.pvalue, options.min_cov, options.max_cov, options.filter_quantil].count(None) == 4:
+        sys.exit('You need to specify at least one filter parameter: --pvalue, --min-coverage, --quantil or --max-coverage')
+    filtering(options.control, options.affected, options.ocontrol, options.oaffected, options.pvalue, options.min_cov, options.max_cov, options.min_delta_methylation, options.filter_quantil)
 
 
 
