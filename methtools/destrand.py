a82d83d4e05aa7aa336b11ac6227be7476ad5a01
Bjoern Gruening <bjoern.gruening@gmail.com>
2013-02-20 11:31:48 +0100

    add --keep-positions

--- methtools/destrand.py
+++ methtools/destrand.py
@@ -4,8 +4,7 @@
 import os, sys
 import argparse
 
-
-def merge_sites(c1, c2):
+def merge_sites(c1, c2, keep_positions = False):
     """
         Merge two lines of a bed file together into one line if they only differ in one position and are located on different strands.
         In that case these two lines represent one symetric CpG and we can merge the coverage and methylation rate.
@@ -17,21 +16,36 @@ def merge_sites(c1, c2):
         c1_cov, c2_cov, c1_meth, c2_meth = map(float, [c1_cov, c2_cov, c1_meth, c2_meth])
         merged_cov = c1_cov + c2_cov
         merged_meth = '%.02f' % ( ((c1_meth * c1_cov) + (c2_meth * c2_cov)) / merged_cov )
-        cm = c1_chrom, c1_start, c1_end, str(merged_cov), merged_meth, c1_strand
+        
+        if keep_positions:
+            start = min(c1_start, c2_start)
+            end = max(c1_end, c2_end)
+
+        if c1_strand == '+':
+            if keep_positions:
+                cm = c1_chrom, start, end, str(merged_cov), merged_meth, c1_strand
+            else:
+                cm = c1_chrom, c1_start, c1_end, str(merged_cov), merged_meth, c1_strand
+        else:
+            if keep_positions:
+                cm = c2_chrom, start, end, str(merged_cov), merged_meth, c2_strand
+            else:
+                cm = c2_chrom, c2_start, c2_end, str(merged_cov), merged_meth, c2_strand
+
         return True, cm
     else:
         c1 = (c1_chrom, c1_start, c1_end, c1_cov, c1_meth, c1_strand)
         return False,c1
 
 
-def merge(sample, outfile):
+def merge(sample, outfile, keep_positions = False):
     previouse_side = None
     for sample_line in sample:
         if not previouse_side:
             previouse_side = sample_line
             continue
         else:
-            merged, merged_sample = merge_sites (previouse_side, sample_line)
+            merged, merged_sample = merge_sites (previouse_side, sample_line, keep_positions)
 
             if merged:
                 previouse_side = None
@@ -56,8 +70,10 @@ def main():
         type=argparse.FileType('w'),
         default=sys.stdout)
 
+    parser.add_argument('-k', '--keep-positions', dest="keep_positions", action='store_true', default=False, help='Keep the position from both sites.')
+
     options = parser.parse_args()
-    merge(options.infile, options.outfile)
+    merge(options.infile, options.outfile, options.keep_positions)
 
 
 if __name__ == '__main__':
